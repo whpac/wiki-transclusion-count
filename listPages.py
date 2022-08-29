11a06891965834abc57e196e8010a68556cdf02b
Marcin Szwarc <marcinszwarc@hotmail.com>
2022-08-29 14:12:55 +0200

    Pages can be filtered with regex

--- listPages.py
+++ listPages.py
@@ -1,4 +1,5 @@
 from time import sleep
+import re
 import requests
 
 def listPages(server, namespace, session=None):
@@ -41,14 +42,19 @@ if __name__ == '__main__':
     from argparse import ArgumentParser
 
     argparser = ArgumentParser()
-    argparser.add_argument('server', metavar='serwer', type=str, required=True, help='adres URL serwera wiki')
-    argparser.add_argument('namespace', metavar='przestrzeń_nazw', type=int, required=True, help='numer przestrzeni nazw, z której listować strony')
+    argparser.add_argument('server', metavar='serwer', type=str, help='adres URL serwera wiki')
+    argparser.add_argument('namespace', metavar='przestrzeń_nazw', type=int, help='numer przestrzeni nazw, z której listować strony')
     argparser.add_argument('--output', '-o', metavar='plik', type=str, help='plik wyjściowy z wynikami')
+    argparser.add_argument('--regex', '-r', metavar='filtr', type=str, help='wyrażenie regularne, do którego muszą pasować nazwy stron')
     args = argparser.parse_args()
     pages = listPages(args.server, args.namespace)
+
+    if args.regex:
+        regex = re.compile(args.regex)
+        pages = filter(lambda page: regex.match(page), pages)
     
     if args.output and args.output != '-':
-        with open(args.output, 'w') as f:
+        with open(args.output, 'w', encoding='utf-8') as f:
             for page in pages:
                 f.write(page + '\n')
     else:
